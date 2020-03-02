2c43554eabb01e29a3d7c704f30d81cd976bfe70
Abel <afacendacarrasco@miami.edu>
2020-03-01 22:29:09 -0500

    wip

--- .docker/init.sh
+++ .docker/init.sh
@@ -6,5 +6,10 @@ echo "Checking Elasticsearch node 1 is ready"
 echo "Checking Elasticsearch node 2 is ready"
 /usr/local/wait-for-it.sh esdata-1.local:9200 -s --timeout=120 -- echo "Elasticsearch node 2 is ready!"
 
+# Installing search app dependencies
+echo "Installing search app dependencies"
+cd /home/site/wwwroot
+composer install
+
 # start apache
 /usr/sbin/apache2ctl -D FOREGROUND
---
+++ search-app/src/Controller/SearchController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('search-app.html.twig');
+    }
+}
