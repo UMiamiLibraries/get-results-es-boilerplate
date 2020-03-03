bed07205648db36840fe55ae7599c3318fc959ad
Abel <axf955@miami.edu>
2020-03-03 14:36:48 -0500

    wip

--- search-app/src/CustomScripts/ElasticsearchIngester.php
+++ search-app/src/CustomScripts/ElasticsearchIngester.php
@@ -1,39 +1,119 @@
 <?php
 
-
 namespace App\CustomScripts;
 
+use Elasticsearch\ClientBuilder;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
+require __DIR__ . '/../../vendor/autoload.php';
 
 class ElasticsearchIngester
 {
-    private $id;
-    private $title;
-    private $online_format;
-    private $location;
-    private $url;
-    private $notes;
-    private $thumbnail_url;
+    private $apiUrl;
+    private $elasticSearchClient;
+    private $indexName;
 
     /**
-     * SearchableObject constructor.
-     * @param $id
-     * @param $title
-     * @param $online_format
-     * @param $location
-     * @param $url
-     * @param $notes
-     * @param $thumbnail_url
+     * ElasticsearchIngester constructor.
+     * @param $apiUrl
+     * @param $indexName
      */
-    public function __construct($id, $title, $online_format, $location, $url, $notes, $thumbnail_url)
+    public function __construct($apiUrl, $indexName)
     {
-        $this->id = $id;
-        $this->title = $title;
-        $this->online_format = $online_format;
-        $this->location = $location;
-        $this->url = $url;
-        $this->notes = $notes;
-        $this->thumbnail_url = $thumbnail_url;
+
+        $this->apiUrl = $apiUrl;
+        $this->indexName = $indexName;
+        $elastic_host_info = [
+            $_ENV['ELASTIC_HOST'],
+            $_ENV['ELASTIC_PORT'],
+            $_ENV['ELASTIC_USER'],
+            $_ENV['ELASTIC_PASSWORD']
+        ];
+        $this->elasticSearchClient = ClientBuilder::create()->setHosts($elastic_host_info)->build();
     }
 
+
+    private function getJsonData($url)
+    {
+        sleep(1); //We are waiting a second between each API call to prevent being temporarily banned by the Library of Congress API
+        $json = file_get_contents($url);
+        return json_decode($json);
+    }
+
+    public function indexExists()
+    {
+        $indexParams['index'] = $this->indexName;
+        return $this->elasticSearchClient->indices()->exists($indexParams);
+    }
+
+    public function createIndex()
+    {
+        $params = [
+            'index' => $this->indexName,
+            'body' => [
+                'settings' => [
+                    'number_of_shards' => 1,
+                    'number_of_replicas' => 2,
+                ],
+            ]
+        ];
+
+        $this->elasticSearchClient->indices()->create($params);
+    }
+
+    public function ingestData()
+    {
+        $progressBar = new ProgressBar(new ConsoleOutput(), 100);
+        echo 'Ingesting data' . PHP_EOL;
+
+        $currentApiUrl = $this->apiUrl;
+        do {
+            $data = $this->getJsonData($currentApiUrl);
+
+            if ($data) {
+                $results = $data->results;
+                $pagination = $data->pagination;
+
+                $params = $this->prepareValues($results);
+
+                $this->elasticSearchClient->bulk($params);
+                $progressBar->advance(4);
+
+                $currentApiUrl = !empty($pagination->next) ? $pagination->next : null;
+            }
+
+        } while (!empty($currentApiUrl));
+
+
+        $progressBar->finish();
+        echo PHP_EOL . 'Done ingesting data' . PHP_EOL;
+
+
+    }
+
+    private function prepareValues($results)
+    {
+        $params = ['body' => []];
+
+        foreach ($results as $result) {
+            $params['body'][] = [
+                'index' => [
+                    '_index' => $this->indexName,
+                    '_id' => $result->id
+                ]
+            ];
+
+            $params['body'][] = [
+                'title' => $result->title,
+                'online_format' => $result->online_format,
+                'location' => $result->location,
+                'url' => $result->url,
+                'notes' => $result->item->notes,
+                'image_url' => $result->image_url
+            ];
+        }
+
+        return $params;
+    }
 
 }
--- search-app/src/CustomScripts/ingest.php
+++ search-app/src/CustomScripts/ingest.php
@@ -2,87 +2,21 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use Symfony\Component\Dotenv\Dotenv;
-use Elasticsearch\ClientBuilder;
 use \App\CustomScripts\ElasticsearchIngester;
+use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../../.env');
 
-$apiUrl = $_ENV['SOURCE_API_URL'];
-$elastic_host_info = [
-    $_ENV['ELASTIC_HOST'],
-    $_ENV['ELASTIC_PORT'],
-    $_ENV['ELASTIC_USER'],
-    $_ENV['ELASTIC_PASSWORD']
-];
-$elasticSearchClient = ClientBuilder::create()->setHosts($elastic_host_info)->build();
-$data = [];
+$apiUrl = 'https://www.loc.gov/collections/national-screening-room/?fo=json';
 
-function getJsonData($url)
-{
-    $json = file_get_contents($url);
-    return json_decode($json);
+$elasticSearchIngester = new ElasticsearchIngester($apiUrl, 'screening-room-index');
+
+if (!$elasticSearchIngester->indexExists()) {
+    $elasticSearchIngester->createIndex();
 }
 
-function indexExists($elasticSearchClient)
-{
-    $indexParams['index'] = 'screening-room-index';
-    return $elasticSearchClient->indices()->exists($indexParams);
-}
+$elasticSearchIngester->ingestData();
 
-function createIndex($elasticSearchClient)
-{
-    $params = [
-        'index' => 'screening-room-index',
-        'body' => [
-            'settings' => [
-                'number_of_shards' => 1,
-                'number_of_replicas' => 2,
-            ],
-        ]
-    ];
 
-    $elasticSearchClient->indices()->create($params);
-}
 
-if (!indexExists($elasticSearchClient)) {
-    createIndex($elasticSearchClient);
-}
-
-do {
-    sleep(1);
-    $data = getJsonData($apiUrl);
-
-    if ($data) {
-        $searchableObjects = [];
-        $results = $data->results;
-        $pagination = $data->pagination;
-
-        $params = ['body' => []];
-
-        foreach ($results as $result) {
-            $params['body'][] = [
-                'index' => [
-                    '_index' => 'screening-room-index',
-                    '_id' => $result->id
-                ]
-            ];
-
-            $params['body'][] = [
-                'title' => $result->title,
-                'online_format' => $result->online_format,
-                'location' => $result->location,
-                'url' => $result->url,
-                'notes' => $result->item->notes,
-                'image_url' => $result->image_url
-
-            ];
-        }
-
-        $elasticSearchClient->bulk($params);
-
-        $apiUrl = !empty($pagination->next) ? $pagination->next : null;
-    }
-
-} while (!empty($apiUrl));
